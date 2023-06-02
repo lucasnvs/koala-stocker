268979f1f37a19fd8922016ff51d82bae31d1286
lucasnvs <lucasli.nev@hotmail.com>
2023-06-01 21:07:11 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const list = document.getElementById('stock-list');
+
+for (let index = 0; index < 20; index++) {
+    var text = `                <li>
+    <div class="item_stock">
+        <img src="./assets/imgs/arroz_namorado.jpg">
+        <h3>Ovos Brancos</h3>
+        <p>Quantidade</p>
+        <p><span class="red">12</span> unidades</p>
+    </div>                 
+</li>`
+    list.innerHTML += text;
+}

d5e7dcd1b1849885bc6bf9cae009ba9c02ccf2d0
lucasnvs <lucasli.nev@hotmail.com>
2023-06-02 14:56:30 -0300

    reformando detalhes

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ for (let index = 0; index < 20; index++) {
     var text = `                <li>
     <div class="item_stock">
         <img src="../assets/imgs/arroz_namorado.jpg">
-        <h3>Ovos Brancos</h3>
+        <h3>Arroz Integral</h3>
         <p>Quantidade</p>
-        <p><span class="red">12</span> unidades</p>
+        <p><span class="red">3.5</span> kg</p>
     </div>                 
 </li>`
     list.innerHTML += text;

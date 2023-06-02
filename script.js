af2ecf23f6920c260c82c022da6374003c1edf66
lucasnvs <lucasli.nev@hotmail.com>
2023-06-02 05:47:40 -0300

    parte do lucas sena e alterações simples de estilo

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const list = document.getElementById('stock-list');
 for (let index = 0; index < 20; index++) {
     var text = `                <li>
     <div class="item_stock">
-        <img src="./assets/imgs/arroz_namorado.jpg">
+        <img src="../assets/imgs/arroz_namorado.jpg">
         <h3>Ovos Brancos</h3>
         <p>Quantidade</p>
         <p><span class="red">12</span> unidades</p>

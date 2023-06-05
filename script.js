fc64d67c0a37eff77f7c86c2806755573846520a
lucasnvs <lucasli.nev@hotmail.com>
2023-06-04 21:09:08 -0300

    adicionando mudanças de estilo no main.html

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const list = document.getElementById('stock-list');
+const groceryList = document.getElementById('grocery-list');
 
 for (let index = 0; index < 20; index++) {
     var text = `                <li>
@@ -10,4 +11,18 @@ for (let index = 0; index < 20; index++) {
     </div>                 
 </li>`
     list.innerHTML += text;
+}
+
+for (let index = 0; index < 20; index++) {
+    var text = `                <li>
+    <div class="grocery-list-card">
+        <h2>Dieta Semanal Vegana</h2>
+        <span>Listar 14 itens <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M98 190.06l139.78 163.12a24 24 0 0036.44 0L414 190.06c13.34-15.57 2.28-39.62-18.22-39.62h-279.6c-20.5 0-31.56 24.05-18.18 39.62z" fill="#FFF"/></svg></span>
+        <ul class="list">
+        </ul>
+        <p>Valor estimado: <span>R$76.80</span></p>
+        <button class="btn">Usar</button>
+    </div>
+</li>`
+    groceryList.innerHTML += text;
 }

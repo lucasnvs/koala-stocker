699c8d5127fa9097ece2e64a54ed694ff666100d
lucasnvs <lucasli.nev@hotmail.com>
2023-06-23 13:27:55 -0300

    card

--- src/client/componentCreation.js
+++ src/client/componentCreation.js
@@ -1,4 +1,4 @@
-function card(title) {
+const card = (title, child = "") => {
     let cardFrame = document.createElement('div');
     cardFrame.className = "card-frame";
 
@@ -21,13 +21,79 @@ function card(title) {
     top_bar.appendChild(cardTitle);
     top_bar.appendChild(btnClose);
     card.appendChild(top_bar);
+    card.appendChild(child);
     cardFrame.appendChild(card);
 
     return cardFrame;
 }
 
-function cardDialog(local) {
-    document.body.appendChild(card("Item novo"));
+function cardDialog() {
+    let body = document.createElement("div");
+    body.className = "product-register";
+
+    let input = document.createElement("input");
+    input.type = "text";
+
+    let texth4 = document.createElement("h4");
+    texth4.textContent = "Qual medida você deseja usar para este alimento?"
+
+    let radioUnit = document.createElement("input");
+    radioUnit.type = "radio";
+    radioUnit.id = "radioUnit";
+    radioUnit.value = "unit";
+    radioUnit.name = "radio-range";
+    let radioKg = document.createElement("input");
+    radioKg.type = "radio";
+    radioKg.id = "radioKg";
+    radioKg.value = "kg";
+    radioKg.name = "radio-range";
+
+    let imageInput = document.createElement("input");
+    imageInput.type = "file";
+
+    let btnSubmit = document.createElement("button");
+    btnSubmit.textContent = "Cadastrar";
+
+    btnSubmit.addEventListener("click", () => {
+        var product;
+        let unit = radioUnit.checked;
+        let kg = radioKg.checked;
+
+        let measureChosen;
+        if(unit) measureChosen = "UNIT";
+        if(kg) measureChosen = "KG";
+
+        let file = imageInput.files[0];
+        var reader = new FileReader();
+        
+        reader.onload = function(event) {
+            var imageContent = event.target.result;
+
+            product = {
+                name: "Leite",
+                measure: measureChosen,
+                image: imageContent,
+                quantRange: { min: 0, max: 10}
+            }
+
+            console.log(product);
+            let im = document.createElement("img");
+            im.src = product.image;
+            document.body.appendChild(im);
+        }
+
+        reader.readAsDataURL(file);
+    })
+
+    body.appendChild(input);
+    body.appendChild(texth4);
+    body.appendChild(radioUnit);
+    body.appendChild(radioKg);
+    body.appendChild(imageInput);
+    body.appendChild(btnSubmit);
+
+    let c = card("Cadastrar alimento", body);
+    document.body.appendChild(c);
 }
 
 const componentCreation = {
